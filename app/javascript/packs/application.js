d0380d39693e9fc5c3957c4f808a0a00d41fc08e
estellegresillon <estelle.gresillon@gmail.com>
2018-06-20 18:52:56 +0200

    exo12 finished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,7 @@ import "bootstrap";
 import { challenge1 } from '../challenges/challenge1';
 const page1 = document.getElementById('challenge1');
 if (page1) { challenge1() }
+
+import { challenge11 } from '../challenges/challenge11';
+const page11 = document.getElementById('challenge11');
+if (page11) { challenge11() }
