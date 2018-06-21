2d2afa62108d6bdc9568a0af370e64d10da96b21
estellegresillon <estelle.gresillon@gmail.com>
2018-06-21 14:48:43 +0200

    exo19 done

--- app/javascript/challenges/challenge19.js
+++
@@ -1,9 +0,0 @@
-function challenge19() {
-  const video = document.querySelector('.player');
-  const canvas = document.querySelector('.photo');
-  const ctx = canvas.getContext('2d');
-  const strip = document.querySelector('.strip');
-  const snap = document.querySelector('.snap');
-}
-
-export { challenge19 }
