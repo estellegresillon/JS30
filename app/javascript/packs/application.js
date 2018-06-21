35631e17c61df4684a7695ed3e3e649ffa158b51
estellegresillon <estelle.gresillon@gmail.com>
2018-06-21 13:09:22 +0200

    exo18 done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,7 @@ if (page1) { challenge1() }
 import { challenge11 } from '../challenges/challenge11';
 const page11 = document.getElementById('challenge11');
 if (page11) { challenge11() }
+
+import { challenge19 } from '../challenges/challenge19';
+const page19 = document.getElementById('challenge19');
+if (page19) { challenge19() }
